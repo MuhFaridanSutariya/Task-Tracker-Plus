4153c33d090cd113bab80d3fd0db3b2c55f62a72
MuhFaridanSutariya <faridansutariya@students.universitasmulia.ac.id>
2024-06-19 13:56:03 +0700

    first init

---
+++ fcp-web-application-v2/middleware/auth.go
@@ -0,0 +1,53 @@
+package middleware
+
+import (
+	"a21hc3NpZ25tZW50/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+)
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
+
+func Auth() gin.HandlerFunc {
+	return gin.HandlerFunc(func(ctx *gin.Context) {
+		tknStr, err := ctx.Cookie("session_token")
+		if err != nil {
+			if ctx.Request.Header.Get("Content-Type") == "application/json" {
+				ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+				return
+			}
+			ctx.Redirect(http.StatusSeeOther, "/login")
+			return
+		}
+
+		claims := &Claims{}
+
+		tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+			return model.JwtKey, nil
+		})
+
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				ctx.AbortWithStatus(http.StatusUnauthorized)
+				return
+			}
+
+			ctx.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		if !tkn.Valid {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		ctx.Set("email", claims.Email)
+
+		ctx.Next()
+	})
+}
